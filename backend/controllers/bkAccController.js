c8c4db465428206009985ea9295e915801d1a8db
LeoTo <leoto0207@gmail.com>
2025-08-14 12:03:33 +1000

    OBS-36 connect to database to delete

--- backend/controllers/bkAccController.js
+++ backend/controllers/bkAccController.js
@@ -138,7 +138,7 @@ const updateBankAccByAccNum = async (req, res) => {
   }
 };
 
-//delete account
+//delete acc
 const deleteBankAcc = async (req, res) => {
   try {
     const bankAcc = await BankAcc.findById(req.params.id);
